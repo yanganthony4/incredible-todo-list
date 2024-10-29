a84baa11bb692552be4a31f605ca3f133cc52bbf
yanganthony4 <yanganthony4@gmail.com>
2024-10-29 12:57:15 -0600

    Created tasks list and map function for tasks list
    Created tasks list using useState
    
    Implemented map function inside ToDoList.jsx to map each item in the tasks list

--- components/app.js
+++ components/app.js
@@ -2,12 +2,15 @@ import { View, Text} from 'react-native'
 import React from 'react'
 import ToDoForm from './ToDoForm'
 import ToDoList from './ToDoList'
+import { useState } from 'react'
 
 
 const App = () => {
+    const [tasks, setTasks] = useState(['Do laundry', 'Go to gym', 'Walk dog'])
+
     return (
         <View>
-            <ToDoList/>
+            <ToDoList tasks={tasks} />
             <ToDoForm/>
         </View>
     )
